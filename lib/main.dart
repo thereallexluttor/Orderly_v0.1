37b15ed5e7197f1bd5bd6ef7dabbfa8bd934aae0
Hedinyer Perucho <alucardperucho@gmail.com>
2024-12-07 09:16:53 +0100

    initial app

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Desactiva el banner
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // Get the current screen size
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        width: screenSize.width, // Use the full width of the screen
+        height: screenSize.height, // Use the full height of the screen
+        color: Colors.white,
+        child: const Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
